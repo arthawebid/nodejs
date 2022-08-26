57f3d39dcb3520534ae5d36ab962e72356a41d57
MhSTIKI <arthawebid@gmail.com>
2022-08-27 07:53:44 +0800

    pengenalan nodejs

--- latihan01.js
+++ latihan01.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var dt = require('./modul01.js');
 
 http.createServer(function(req,res){
 	res.writeHead(200,{'content-type':'text/html'});
-	res.end('<h2>Belajar NodeJS</h2>Cara menggunakan node JS dan menggunakan modul<br>'+ dt.TulisPesan());
+	res.end('<h2>Belajar NodeJS</h2>Cara menggunakan node JS<br>');
 }).listen(8080);
+
 console.log('Server ready');
